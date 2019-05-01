47f3ef5947b418107ef07eecfb5cb0b6e2923014
JiMaC <jimac221@gmail.com>
2019-05-01 11:11:38 +0300

    homework 04

--- Lessons/04/Homework/Litres-Counter/Litres-Counter/Program.cs
+++ Lessons/04/Homework/Litres-Counter/Litres-Counter/Program.cs
@@ -9,39 +9,48 @@ namespace Litres_Counter
             None = 0,
             BigBox = 20,
             MidBox = 5,
-            SmallBox = 3
+            SmallBox = 1
         }
 
 
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Введите количество литров, которые нужно упаковать:");
-            var volume = Math.Floor(Convert.ToDouble(Console.ReadLine()));
-            var bigCont = Math.Floor((double)Box.BigBox);
+            try
+            {
+                Console.WriteLine("Введите количество литров, которые нужно упаковать:");
+            var volume = Math.Ceiling(Convert.ToDouble(Console.ReadLine()));
+            var bigCont = Math.Ceiling(Convert.ToDouble(Box.BigBox));
             var bigOst = volume % (double)Box.BigBox;
-            var midCont = Math.Floor((double)Box.MidBox);
+            var midCont = Math.Ceiling(Convert.ToDouble(Box.MidBox));
             var midOst = bigOst % (double)Box.MidBox;
-            var smallCont = Math.Ceiling((double)Box.SmallBox);
-            var smallOst = midOst % (double)Box.SmallBox;
+            var smallCont = Math.Ceiling(Convert.ToDouble(Box.SmallBox));
 
             Console.WriteLine($"Вы ввели {volume}");
 
 
-                if (volume >= bigCont)              
+
+                if (volume >= bigCont)
                 {
-                Console.WriteLine($"Вам необходимо {volume/bigCont} , 20 литровых контейнеров");
+                    Console.WriteLine($"Вам необходимо {Math.Floor(volume / bigCont)} , 20 литровых контейнеров");
                 }
 
-                if (volume >= midCont)
+                if (bigOst >= midCont)
                 {
-                    Console.WriteLine($"Вам необходимо {volume / midCont} , 5 литровых контейнеров");
+                    Console.WriteLine($"Вам необходимо {Math.Floor(bigOst / midCont)} , 5 литровых контейнеров");
                 }
 
-
-                if (volume >= smallCont)
+                if (midOst >= smallCont)
                 {
-                Console.WriteLine($"Вам небходимо {volume / smallCont} , 3 литровых контейнеров");
+                    Console.WriteLine($"Вам небходимо {Math.Ceiling(midOst / smallCont)} , 1 литровых контейнеров");
                 }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine($"Не верный формат введеного значения!");
+            }
+               
 
             Console.ReadKey();
         }
