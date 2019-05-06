5605fbd5a07ad84b279fff79f1e348a0ac3333b7
Direaper <jimac221@gmail.com>
2019-05-06 10:59:50 +0400

    new version

--- Lessons/05/HomeWork/Figures/Figures/Figures/Program.cs
+++ Lessons/05/HomeWork/Figures/Figures/Figures/Program.cs
@@ -6,9 +6,9 @@ namespace Figures
     {
         enum Figures
         {
-            rectangle = 1,
-            triagle, 
-            circle
+            Rectangle = 1,
+            Triagle, 
+            Circle
         }
         static void Main(string[] args)
         {
@@ -26,39 +26,46 @@ namespace Figures
                 var getValue = (Figures)Enum.Parse(typeof(Figures), input);
                 Console.WriteLine($"you choose:  {getValue}");
 
-                if ((int)getValue == 1)
+                switch (Convert.ToInt32(input))
                 {
-                    Console.WriteLine("Please, enter the height of the rectangle: ");
-                    double height = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Please, enter the width of the rectangle: ");
-                    double width = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine($"The area of the rectangle: {height * width}");
-                    Console.WriteLine($"The perimeter of the rectangle: {2 * (height + width)}");
+                    case 1:
+                        Console.WriteLine("Please, enter the height of the rectangle: ");
+                        double height = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Please, enter the width of the rectangle: ");
+                        double width = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"The area of the rectangle: {height * width}");
+                        Console.WriteLine($"The perimeter of the rectangle: {2 * (height + width)}");
+                        break;
+                    case 2:
+                        Console.WriteLine("Please, enter the side of the triangle: ");
+                        double side = Convert.ToDouble(Console.ReadLine());
+                        double sqrt = Math.Sqrt(3);
+                        double squareSide = Math.Round((sqrt / 4) * (side * side), 2);
+                        Console.WriteLine($"The area of the triangle: {squareSide}");
+                        Console.WriteLine($"The perimetr of the triangle: {side * 3}");
+                        break;
+                    case 3:
+                        Console.WriteLine("Please, enter the diametr of the circle: ");
+                        double P = Math.Round(Math.PI, 2);
+                        double Diametr = Convert.ToDouble(Console.ReadLine());
+                        double area = (P / 4) * (Diametr * Diametr);
+                        Console.WriteLine($"The area of the circle: {area}");
+                        Console.WriteLine($"The perimetr of the circle: {P * Diametr}");
+                        break;
+                   default:
+                        Console.WriteLine($"Sorry, you enter the wrong value of the figure!");
+                        break;
+
                 }
 
-                if ((int)getValue == 2)
-                {
-                    Console.WriteLine("Please, enter the side of the triangle: ");
-                    double side = Convert.ToDouble(Console.ReadLine());
-                    double sqrt = Math.Sqrt(3);
-                    double squareSide = Math.Round((sqrt / 4) * (side * side), 2);
-                    Console.WriteLine($"The area of the triangle: {squareSide}");
-                    Console.WriteLine($"The perimetr of the triangle: {side * 3}");
-                }
-
-                if ((int)getValue == 3)
-                {
-                    Console.WriteLine("Please, enter the diametr of the circle: ");
-                    double P = 3.14;
-                    double Diametr = Convert.ToDouble(Console.ReadLine());
-                    double area = (P / 4) * (Diametr * Diametr);
-                    Console.WriteLine($"The area of the circle: {area}");
-                    Console.WriteLine($"The perimetr of the circle: {P * Diametr}");
-                }
-            }
-            catch (SystemException)
+             }
+            catch (System.ArgumentException)
             {
                 Console.WriteLine($"You enter wrong value!");
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine($"You enter wrong format!");
             }
             Console.ReadKey();
         }
