69291d0f723f536f2b27fbac00a807e1f9106996
Direaper <jimac221@gmail.com>
2019-05-23 15:15:48 +0400

    StringApp2

---
+++ Lessons/07/HomeWork/StringApp2/StringApp2/StringApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку из нескольких слов");
+            var input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            int len = words.Length;
+
+            for (int i = len; i > 0; i--)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
