2ae5c24a449718c2fb8e0f39025a399e20b91899
JiMaC <jimac221@gmail.com>
2019-05-24 00:27:19 +0300

    stringApp update

--- Lessons/07/HomeWork/StringApp2/StringApp2/StringApp2/Program.cs
+++ Lessons/07/HomeWork/StringApp2/StringApp2/StringApp2/Program.cs
@@ -8,13 +8,35 @@ namespace StringApp2
         {
             Console.WriteLine("Введите строку из нескольких слов");
             var input = Console.ReadLine();
-            string[] words = input.Split(' ');
-            int len = words.Length;
+            string[] values = new string[input.Length];
+            string[] temp = new string[input.Length];
 
-            for (int i = len; i > 0; i--)
+            for (int i = 0; i < input.Length; i++)
             {
-                Console.WriteLine(words[i]);
+                values[i] = Convert.ToString(input[i]);
+                //Console.Write($"{values[i]}");
             }
+
+            Console.WriteLine($"\n part two\n");
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                for (int j = 1; j < values.Length; j++)
+                {
+                    temp[i] = values[i];
+                    values[i] = values[j];
+                    values[j] = temp[i];
+
+                }  
+                
+            } 
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.Write($"{values[i]}");
+
+            }
+
 
             Console.ReadKey();
         }
