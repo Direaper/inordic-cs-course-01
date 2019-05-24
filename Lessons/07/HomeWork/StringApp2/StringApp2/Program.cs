029b760d1a2b3f7eb6c72637b62c87f20f173e9c
Direaper <jimac221@gmail.com>
2019-05-24 11:38:00 +0400

    add

---
+++ Lessons/07/HomeWork/StringApp2/StringApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите строку из нескольких слов");
+            var input = Console.ReadLine();
+            string[] values = new string[input.Length];
+            string[] temp = new string[input.Length];
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                values[i] = Convert.ToString(input[i]);
+            }
+
+
+            for (int i = 0, j = values.Length - 1; j >= 0; j--, i++)
+            {
+                temp[i] = values[j];
+            }
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.Write($"{temp[i].ToLower()}");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
