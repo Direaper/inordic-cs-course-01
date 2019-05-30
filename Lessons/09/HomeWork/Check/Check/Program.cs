51f321b6d1757e67ce8c2ae32e39eccdd3e1415a
JiMaC <jimac221@gmail.com>
2019-05-30 22:52:56 +0300

    08 and 09 homework

---
+++ Lessons/09/HomeWork/Check/Check/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Введите скобки на проверку.");
+                var find = Console.ReadLine();
+                var value = 0;
+                var value2 = 0;
+                var value3 = 0;
+
+                foreach (var words in find)
+                {
+                    if (words == '(')
+                    {
+                        value++;
+                    }
+                    else if (words == ')')
+                    {
+                        value--;
+                    }
+                    else if (words == '{')
+                    {
+                        value2++;
+                    }
+                    else if (words == '}')
+                    {
+                        value2--;
+                    }
+                    else if (words == '[')
+                    {
+                        value3++;
+                    }
+                    else if (words == ']')
+                    {
+                        value3--;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Вы ввели иное значение");
+                        value--;
+                    }
+                }
+
+                if ((value|value2|value3) == 0)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+
+                Console.WriteLine("Программа завершена, нажмите любую клавишу, чтобы продолжить");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
