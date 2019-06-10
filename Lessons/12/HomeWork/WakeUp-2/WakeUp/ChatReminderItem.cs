e2de64a0d823ab0275a2b4c95fb5a5ddc41a394e
JiMaC <jimac221@gmail.com>
2019-06-10 22:48:18 +0300

    wake-up-2

---
+++ Lessons/12/HomeWork/WakeUp-2/WakeUp/ChatReminderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WakeUp
+{
+    public class ChatReminderItem : ReminderItem
+    {
+        public string ChatName { get; set; }
+        public string AccountName { get; set; }
+
+        public ChatReminderItem(string chatname, string accountname, string alarmMessage, DateTimeOffset alarmDate) : base(alarmMessage, alarmDate)
+        {
+            ChatName = chatname;
+            AccountName = accountname;
+        }
+        public override string WriteProperties()
+        {
+            return $"Название чата: {ChatName}\n Аккаунт: {AccountName}\n Сообщение:{AlarmMessage}\n Дата будильника: {AlarmDate}\n Текущее время: {TimeToAlarm}\n Просрочено ли событие: {IsOutdated}\n Тип:{GetType()}";
+        }
+
+    }
+}
---
+++ Lessons/12/HomeWork/WakeUp-2/WakeUp/ReminderItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WakeUp
+{
+    public class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm { get { return AlarmDate - DateTime.Now; } set { } }
+        public bool IsOutdated
+        {
+            get
+            {
+                if (TimeToAlarm == TimeSpan.Zero)
+                {
+                   return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            set { } }
+
+
+        public ReminderItem(string alarmMessage, DateTimeOffset alarmDate)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public virtual string WriteProperties()
+        {
+             return $"Сообщение:{AlarmMessage}\n Дата будильника: {AlarmDate}\n Текущее время: {TimeToAlarm}\n Просрочено ли событие: {IsOutdated}\n Тип:{GetType()}"; 
+        }
+    }
+}
