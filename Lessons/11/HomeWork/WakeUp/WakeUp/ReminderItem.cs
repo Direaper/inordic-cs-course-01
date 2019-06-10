ce6a4d9cee45f1f470ba200d5efad3198209acab
Direaper <jimac221@gmail.com>
2019-06-10 10:58:38 +0400

    wake-up update

---
+++ Lessons/11/HomeWork/WakeUp/WakeUp/ReminderItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WakeUp
+{
+    public class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm { get { return AlarmDate - DateTime.Now; } set { } }
+        public bool IsOutdated
+        {
+            get
+            {
+                if (TimeToAlarm == TimeSpan.Zero)
+                {
+                   return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            set { } }
+
+
+        public ReminderItem(string alarmMessage, DateTimeOffset alarmDate)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public string WriteProperties()
+        {
+             return $"Сообщение:{AlarmMessage}, Дата будильника: {AlarmDate}, Текущее время: {TimeToAlarm}, Просрочено ли событие: {IsOutdated}"; 
+        }
+
+ 
+
+    }
+
+}
