0abe450e6000c978fcf29a6264525cecbc7b48be
Direaper <jimac221@gmail.com>
2019-08-23 13:20:08 +0400

    newStringApp

---
+++ Lessons/07/HomeWork/NewStringApp/StringApp-new/StringApp-new/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringApp_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите любое слово для инверсии");
+                string value = Console.ReadLine();
+
+                string correctionFormat = value.ToLower();
+                for (int b = correctionFormat.Length; b > 0; b--)
+                {
+                    Console.Write(correctionFormat[b - 1]);
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
+
