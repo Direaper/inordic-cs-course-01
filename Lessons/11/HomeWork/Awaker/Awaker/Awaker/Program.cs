2ecac54f7aff89b0c6dca0d268f30101842de56f
JiMaC <jimac221@gmail.com>
2019-09-08 00:06:26 +0300

    awaker

---
+++ Lessons/11/HomeWork/Awaker/Awaker/Awaker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Awaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTimeOffset datetime = DateTimeOffset.Parse("2019-09-07 23:47");
+            ReminderItem bud1 = new ReminderItem(datetime, "ALARM");
+            bud1.WriteProperties();
+
+            DateTimeOffset datetime2 = DateTimeOffset.Parse("2019-09-07 12:47");
+            ReminderItem bud2 = new ReminderItem(datetime, "ALARM");
+            bud2.WriteProperties();
+        }
+    }
+}
---
+++ Lessons/11/HomeWork/Awaker/Awaker/Awaker/ReminderItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Awaker
+{
+    class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm
+        {
+            get
+            {
+                var TimeNow = DateTime.Now;
+                TimeSpan value = TimeNow - AlarmDate; //TimeNow - AlarmDate;
+                return value;
+            }
+        }
+
+        public bool IsOutdated
+        {
+            get
+            {
+                return DateTime.Now >= AlarmDate;
+            }
+        }
+        public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+
+        public void WriteProperties()
+        {
+            Console.WriteLine($"Alarm Date: {AlarmDate} \nAlarm Message: {AlarmMessage} \nTime to Alarm: {TimeToAlarm} \nIs out Date {IsOutdated} \n");
+        }
+    }
+}
