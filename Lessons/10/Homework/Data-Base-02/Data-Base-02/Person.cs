b618d6f44bad3e66ba52c3d886836648d213c51d
JiMaC <jimac221@gmail.com>
2019-09-08 00:29:58 +0300

    Полностью переделан класс Person. добавлены циклы в основной класс programm. Вроде сейчас все должно быть правильно

---
+++ Lessons/10/Homework/Data-Base-02/Data-Base-02/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int AgeFour { get; }
+        public string Information
+        {
+            get
+            {
+                return $"Name: {Name} \n Age{Age} \nage in 4 years: {AgeFour}";
+            }
+        }
+    }
+}
+
+
