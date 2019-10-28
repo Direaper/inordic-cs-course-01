b65ecd59adb828f1a72d399abf5a6eb4096ea18d
JiMaC <jimac221@gmail.com>
2019-10-28 12:00:05 +0300

    реализация синглтона

---
+++ Lessons/14/HomeWork/Logger/Logger/Logger/MultipleLogWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    class MultipleLogWriter  : BaseLogWriter
+    {
+        IList<ILogWriter> _logWriters;
+        private static MultipleLogWriter _multiplyLogWriter;
+        private MultipleLogWriter(IList<ILogWriter> logWriters)
+        {
+            _logWriters = logWriters;
+        }
+
+        public static MultipleLogWriter GetInstatance()
+        {
+            if (_multiplyLogWriter == null)
+                _multiplyLogWriter = new MultipleLogWriter(new List<ILogWriter>());
+            return _multiplyLogWriter;
+        }
+
+        public override void LogSingleRecord(LogMessageType logMessageType, string message)
+        {
+            foreach (var logWriter in _logWriters)
+            {
+                logWriter.LogSingleRecord(logMessageType, message);
+            }
+        }
+        public override void Dispose()
+        {
+            foreach (var logWriter in _logWriters)
+            {
+                if (logWriter != null)
+                    logWriter.Dispose();
+            }
+        }
+    }
+}
